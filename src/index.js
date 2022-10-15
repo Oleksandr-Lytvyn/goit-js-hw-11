d9495b406d1a13398528bff5ded89e503ba15301
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-10-15 15:32:02 +0300

    15-10

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { refs } from './scripts/refs';
 import { fetchImages } from './scripts/fetchImages';
 import { createMarkup } from './scripts/createMarkup';
@@ -11,35 +14,72 @@ refs.form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   unsplashApi.resetPage();
+  refs.cardsList.innerHTML = '';
 
   const { searchQuery } = event.target;
   const query = searchQuery.value.trim().toLowerCase();
 
   unsplashApi.query = query;
-  unsplashApi.getPhotos().then(({ hits, total, totalHits }) => {
-    Notify.success(`Hooray! We found ${totalHits} images.`, {
-      closeButton: true,
+  unsplashApi
+    .getPhotos()
+    .then(({ hits, total, totalHits }) => {
+      if (total > 0) {
+        Notify.success(`Hooray! We found ${totalHits} images.`, {
+          closeButton: true,
+        });
+      }
+      if (total === 0) {
+        console.log('total=0');
+        Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+
+      const markup = createMarkup(hits);
+      refs.cardsList.insertAdjacentHTML('beforeend', markup);
+      unsplashApi.calculateTotalPages(total);
+      if (unsplashApi.isNotLastPage) {
+        refs.loadBtn.classList.remove('is-hidden');
+      }
+    })
+    .catch(error => {
+      Notify.failure(error);
+      unsplashApi.resetPage();
+      refs.cardsList.innerHTML = '';
     });
-    createMarkup(hits);
-    unsplashApi.calculateTotalPages(total);
-    if (unsplashApi.isNotLastPage) {
-      refs.loadBtn.classList.remove('is-hidden');
-    }
-  });
+  // console.log('after create markup');
 }
 
 refs.loadBtn.addEventListener('click', onLoadMore);
 
 function onLoadMore() {
   unsplashApi.incrementPage();
-  unsplashApi.getPhotos().then(({ hits }) => {
-    createMarkup(hits);
-  });
-  if (!unsplashApi.isNotLastPage) {
-    Notify.info("We're sorry, but you've reached the end of search results.");
-    refs.loadBtn.classList.add('is-hidden');
-  }
+  unsplashApi
+    .getPhotos()
+    .then(({ hits }) => {
+      const markup = createMarkup(hits);
+      refs.cardsList.insertAdjacentHTML('beforeend', markup);
+      if (!unsplashApi.isNotLastPage) {
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        refs.loadBtn.classList.add('is-hidden');
+      }
+    })
+    .catch(error => {
+      Notify.failure(error);
+      unsplashApi.resetPage();
+      refs.cardsList.innerHTML = '';
+    });
 }
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'top',
+  captionType: 'attr',
+  captionDelay: 250,
+  showCounter: false,
+});
 
 // refs.form.addEventListener('submit', onSubmit);
 
--- src/scripts/createMarkup.js
+++ src/scripts/createMarkup.js
@@ -2,15 +2,36 @@ import { refs } from './refs';
 
 export function createMarkup(hits) {
   //   clearCardList();
-  let dataMarkup = '';
-  hits.map(element => {
-    dataMarkup += createCard(element);
-  });
+  // let dataMarkup = '';
+  return hits
+    .map(element => {
+      return (refs.cardsList.innerHTML = `
+  <a  href=${element.largeImageURL}">
+    <img src="${element.webformatURL}" alt="${element.tags}" width="150px" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>${element.likes} Likes</b>
+      </p>
+      <p class="info-item">
+        <b>${element.views} Views</b>
+      </p>
+      <p class="info-item">
+        <b>${element.comments} Comments</b>
+      </p>
+      <p class="info-item">
+        <b>${element.downloads} Downloads</b>
+      </p>
+    </div>
+    </a >
+    `);
+    })
+    .join('');
   refs.cardsList.innerHTML = dataMarkup;
 }
 
 function createCard(dataCard) {
-  return `<div class="photo-card">
+  return `
+  <a  href=${dataCard.largeImageURL}">
     <img src="${dataCard.webformatURL}" alt="${dataCard.tags}" width="150px" loading="lazy" />
     <div class="info">
       <p class="info-item">
@@ -26,9 +47,6 @@ function createCard(dataCard) {
         <b>${dataCard.downloads} Downloads</b>
       </p>
     </div>
-  </div>`;
+    </a >
+    `;
 }
-
-// function clearCardList() {
-//   refs.cardsList.innerHTML = '';
-// }
